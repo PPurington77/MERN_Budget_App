226090a3f11cb17c691e3a4e73cb7fa5e7b77cdb
PPurington77 <papurington@gmail.com>
2022-09-21 12:08:59 -0600

    Server.js updated

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const port = 8000;
-const { expenseRouter } = require('./routes/expense.routes');
+const { projectRouter } = require('./routes/routes');
+
 const app = express();
 require('./config/mongoose.config');
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/expenses', expenseRouter);
+app.use('/api', projectRouter);
 
 app.listen(port, () => 
     console.log(`Listening on port ${ port }`)
